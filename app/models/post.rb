b3e1afc5d03fc8415b4f6a3ae9ed81546be87d2a
ryan m <contact@ryanmeek.com>
2020-09-22 19:59:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validates :content, length: { is: 100 }, allow_blank: true
 end
