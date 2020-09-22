50c13ce15cb0677a4000b5635f8f7d4f9837d925
ryan m <contact@ryanmeek.com>
2020-09-22 20:09:51 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,7 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-    redirect_to author_path(@author) if @author.save else render :new
+    @author.save ? redirect_to author_path(@author) : render :new
   end
 
   private
